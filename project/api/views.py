b8d46825ca9a06ca64b471b502b3c388eea0bba6
Kristian Jakobsen <nymannjakobsen@gmail.com>
2019-03-16 14:16:59 +0100

    Removed datatable

--- project/api/views.py
+++ project/api/views.py
@@ -7,18 +7,6 @@ from project.api import api
 from project.models import DB, Case, Holiday
 
 
-@api.route('/cases')
-def cases():
-    cases = DB.session.query(
-        Case.id.label("id"),
-        Case.date.label("date"),
-        Case.name.label("name")
-    ).filter(
-        Case.date >= datetime.now()
-    ).order_by(asc("date")).all()
-    return jsonify(cases=cases)
-
-
 @api.route("/holiday")
 def holiday():
     """
--- project/site/views.py
+++ project/site/views.py
@@ -1,11 +1,22 @@
-from flask import render_template
+from datetime import datetime
 
+from flask import render_template
+from sqlalchemy import asc
+
+from project.models import DB, Case
 from project.site import site
 
 
 @site.route('/')
 def index():
-    return render_template('site/index.html')
+    cases = DB.session.query(
+        Case.id.label("id"),
+        Case.date.label("date"),
+        Case.name.label("name")
+    ).filter(
+        Case.date >= datetime.now()
+    ).order_by(asc("date")).all()
+    return render_template('site/index.html', cases=cases)
 
 
 @site.route('/error')
